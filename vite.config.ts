4589f29ed9e1da0ab14ae6da0c05727a7e079fe6
The pixel man <836520653@qq.com>
2025-03-05 18:04:11 +0800

    myWebsite

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
-  base: '/vue/',
+  base: process.env.NODE_ENV === 'production' ? '/vue/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
